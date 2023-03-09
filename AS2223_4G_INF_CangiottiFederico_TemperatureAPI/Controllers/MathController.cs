9319241d494e6ea6ab3c40d7160af96d95f34bd8
Federico Cangiotti <13657090-federicocangiotti@users.noreply.gitlab.com>
2023-03-09 16:43:32 +0100

    feat: Add MathController

---
+++ AS2223_4G_INF_CangiottiFederico_TemperatureAPI/Controllers/MathController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AS2223_4G_INF_CangiottiFederico_TemperatureAPI.Controllers
+{
+    [ApiController]
+    [Produces("application/json")]
+    [Route("api/v1/")]
+    public class MathController : ControllerBase
+    {
+    }
+}
