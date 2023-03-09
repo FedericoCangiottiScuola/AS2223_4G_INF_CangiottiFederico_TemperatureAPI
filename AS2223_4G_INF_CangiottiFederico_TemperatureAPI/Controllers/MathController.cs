67a4a36604ae5adb316d872580362b30b2323a68
Federico Cangiotti <13657090-federicocangiotti@users.noreply.gitlab.com>
2023-03-09 16:53:02 +0100

    feat: Add Math model class

--- AS2223_4G_INF_CangiottiFederico_TemperatureAPI/Controllers/MathController.cs
+++ AS2223_4G_INF_CangiottiFederico_TemperatureAPI/Controllers/MathController.cs
@@ -7,5 +7,10 @@ namespace AS2223_4G_INF_CangiottiFederico_TemperatureAPI.Controllers
     [Route("api/v1/")]
     public class MathController : ControllerBase
     {
+        [HttpGet("GetAddition")]
+        public JsonResult GetAddition(int n1, int n2)
+        {
+
+        }
     }
 }
---
+++ AS2223_4G_INF_CangiottiFederico_TemperatureAPI/Math.cs
@@ -0,0 +1,20 @@
+﻿namespace AS2223_4G_INF_CangiottiFederico_TemperatureAPI
+{
+    public enum Status
+    {
+        OK,
+        ERROR
+    }
+
+    public class MathResponse
+    {
+        public Status status { get; set; }
+        public string message { get; set; }
+
+        public MathResponse(Status status, string message)
+        {
+            this.status = status;
+            this.message = message;
+        }
+    }
+}
