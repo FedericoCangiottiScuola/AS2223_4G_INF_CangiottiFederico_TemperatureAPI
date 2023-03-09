22d4cbd6b3dcb1389342d84b6a2d58f719b15f75
Federico Cangiotti <13657090-federicocangiotti@users.noreply.gitlab.com>
2023-03-09 17:14:33 +0100

    feat: Change inherited class for MathController

--- AS2223_4G_INF_CangiottiFederico_TemperatureAPI/Controllers/MathController.cs
+++ AS2223_4G_INF_CangiottiFederico_TemperatureAPI/Controllers/MathController.cs
@@ -5,7 +5,7 @@ namespace AS2223_4G_INF_CangiottiFederico_TemperatureAPI.Controllers
     [ApiController]
     [Produces("application/json")]
     [Route("api/v1/")]
-    public class MathController : ControllerBase
+    public class MathController : Controller
     {
         [HttpGet("GetAddition")]
         public JsonResult GetAddition(int n1, int n2)
