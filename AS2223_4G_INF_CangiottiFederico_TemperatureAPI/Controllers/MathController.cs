f9e9685c7a3bf3c49dde5689c3065d5e90c03dd6
Federico Cangiotti <13657090-federicocangiotti@users.noreply.gitlab.com>
2023-03-09 17:15:13 +0100

    feat: Add GetAddition method

--- AS2223_4G_INF_CangiottiFederico_TemperatureAPI/Controllers/MathController.cs
+++ AS2223_4G_INF_CangiottiFederico_TemperatureAPI/Controllers/MathController.cs
@@ -10,7 +10,11 @@ namespace AS2223_4G_INF_CangiottiFederico_TemperatureAPI.Controllers
         [HttpGet("GetAddition")]
         public JsonResult GetAddition(int n1, int n2)
         {
-
+            MathResult outcome = new(Status.OK);
+            return Json(new {
+                result = n1 + n2,
+                outcome,
+            });
         }
     }
 }
